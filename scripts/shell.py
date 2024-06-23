f61dcb04ef4793375c06715cd36f0b2f8de8fa4b
FCzajkowski <franiu325@gmail.com>
2024-06-23 16:42:12 +0200

    Update shell.py

--- scripts/shell.py
+++ scripts/shell.py
@@ -1,8 +1,21 @@
 import basic
+import platform
+import sys
+import datetime
 
+date_and_hour = datetime.datetime.now()
+version_of_platform = platform.system()
+
+print(f"""
+gentle 0.1 (dev ver) | {date_and_hour.strftime('%H:%M:%S')} | {version_of_platform}
+""")
+for i in range(2):print("")
 while True:
-    text = input('Gentle >> ')
+    text = input('[G]>> ')
     result, error = basic.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
+    if text == "":pass
+    else:
+        if text.lower() in ["exit", "exit "]:exit()
+        else:
+            if error: print(error.as_string())
+            else:print(result)
